8def2dea11fded049e931bff0675440d298a5cfd
Luke Sampson <luke.r.sampson@gmail.com>
2025-11-10 12:21:29 -0700

    Added Model and ViewModel files. Changed functionality of game and cleaned up code.

--- Memorize/Memorize/View/ContentView.swift
+++ Memorize/Memorize/View/ContentView.swift
@@ -11,59 +11,22 @@ struct ContentView: View {
     
     let emojis: [String] = ["😎", "😝", "😉", "🤓", "🤣", "😏", "🤫", "🤠", "😴", "🤮", "🫨", "🥹"]
     
-    @State var cardCount: Int = 4
-
     var body: some View {
-        VStack {
-            ScrollView {
-                cards
-            }
-            Spacer()
-            cardCountAdjusters
+        ScrollView {
+            cards
         }
         .padding()
     }
     
-    // MARK: Card Count Adjusters UI
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-
-    // MARK: Cards UI
+    // MARK: Cards
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
+            ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.blue)
-    }
-    
-    // MARK: Card Count Adjuster Function
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-                cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-
-    // MARK: Card Remover
-    var cardRemover: some View {
-        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    
-    // MARK: Card Adder
-    var cardAdder: some View {
-        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
---
+++ Memorize/Memorize/ViewModel/EmojiMemoryGame.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Luke Sampson on 11/10/25.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemoryGame<String>
+    
+    init(model: MemoryGame<String>) {
+        self.model = model
+    }
+}
